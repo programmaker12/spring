79a537e3c0d3ee4befc37f80511cfc4d57f35c5c
Yash Chavan <Yash Chavan@DESKTOP-IOCESD2>
2023-08-09 20:57:56 +0530

    Added SpringIOC1

---
+++ SpringIOC1/src/main/java/com/tns/spring/demo/Jio.java
@@ -0,0 +1,14 @@
+package com.tns.spring.demo;
+
+public class Jio implements Sim {
+    
+	public void calling()
+    {
+    System.out.println("calling JIO.......");	
+    }
+	
+    public void data()
+    {
+    	System.out.println("Searching JIO......");
+    }
+}
---
+++ SpringIOC1/src/main/java/com/tns/spring/demo/Sim.java
@@ -0,0 +1,7 @@
+package com.tns.spring.demo;
+
+public interface Sim {
+	void calling();
+	void data();
+
+}

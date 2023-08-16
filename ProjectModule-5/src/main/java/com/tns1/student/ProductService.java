b633520ec81f124eb23e716badad8957008c0d23
programmaker12 <139685764+programmaker12@users.noreply.github.com>
2023-08-16 16:33:57 +0530

    Added ProjectModule-5

---
+++ ProjectModule-5/src/main/java/com/tns1/student/ProductService.java
@@ -0,0 +1,34 @@
+package com.tns1.student;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repo;
+	
+	public List<Product> listAll()
+	{
+		return repo.findAll();
+	}
+	
+	public void save(Product product)
+	{
+		repo.save(product);
+	}
+	public Product get(Integer id)
+	{
+		return repo.findById(id).get();
+	}
+	public void delete(Integer id)
+	{
+		repo.deleteById(id);
+	}
+}
---
+++ ProjectModule-5/src/test/java/com/tns1/student/ProjectModule5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tns1.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectModule5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
